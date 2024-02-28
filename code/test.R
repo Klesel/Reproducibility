5f52429fb2815651d9a92d2f2cf8e5a8dd6cfd7e
Michael Klesel <michael@klesel.info>
2024-02-28 16:02:36 +0100

    update: add languageserver

--- code/test.R
+++ code/test.R
@@ -1,8 +1,22 @@
-# library(languageserver)
+# ---------------------------------------------------------------
+# This is a simplistic example to test reproducible code using R. 
+#
+# Note that using the R Extension in VS Code requires 
+# the languageserver package. More Information can be found here:
+# https://code.visualstudio.com/docs/languages/r
+# 
+#  Because we are using renv, it needs to be loaded to be a
+# accessible for the extension.
+# ---------------------------------------------------------------
+
+
+library(languageserver)
 library(psych)
 library(conflicted)
 
 conflicts_prefer(psych::describe)
 # Code
 vector <- c(1, 2, 3, 4, 5, 6, 7)
-describe(vector)
+
+print(describe(vector))
+
