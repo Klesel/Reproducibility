4e0789a3739c8de0c69d081838852654d609af2d
Michael Klesel <michael@klesel.info>
2024-02-28 15:08:00 +0100

    add: example with conflicted

--- code/test.R
+++ code/test.R
@@ -1,14 +1,8 @@
-# 1. Make sure you start R console or radian first
-
-# 2. renv::restore()
-
-# Load packages
 library(languageserver)
-library(jsonlite)
 library(psych)
+library(conflicted)
 
+conflicts_prefer(psych::describe)
 # Code
 vector <- c(1, 2, 3, 4, 5, 6, 7)
-psych::describe(vector)
-
-vector |> describe()
+describe(vector)
